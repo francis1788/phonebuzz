1eec008449adfae23be54bfa7138ee50e57bea49
Francis Kumar <abishek1788@Franciss-MacBook-Pro.local>
2018-01-15 06:54:17 -0800

    pushed

---
+++ README.md
@@ -0,0 +1,31 @@
+# PhoneBuzz
+
+PhoneBuzz is a webapplication that uses the Twilio API to call a user, receive a numerical input from the user, and then recite the FizzBuzz sequence up to that input unto the user. To use this application one can call into the application, use the application to create an outgoing call, or replay a previous outgoing call in the call history. This application was built in response to the LendUp coding challenge.
+
+## Try It Out
+
+You can try out the app [here](https://phonebuzz-francis.herokuapp.com/). The "Call From PhoneBuzz option does not work because I used a free Twilio account to make the website."
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. See deployment for notes on how to deploy the project on a live system.
+
+### Prerequisites
+
+You will need the Flask and Twilio libraries/frameworks within python3. Look at the requirements.txt file for more information. Either of these can be easily installed through "pip install twilio/flask"
+
+
+## Running the application on your own
+
+To get the app up and running on your own machine, with your own Twilio account you will need to go to the app.py file, and input your Twilio credentials into the first few lines of code after the import statements:
+```
+TWILIO_ACCOUNT_SID = ______________
+TWILIO_AUTH_TOKEN = _______________
+TWILIO_CALLER_ID = ________________
+```
+Then you will need to change this line in app.py to the url which you will be hosting your website off of. 
+```
+url = _________________
+```
+## Thank You!
+For any questions email me at (francis1788@gmail.com)
---
+++ app.py
@@ -0,0 +1,213 @@
+import os
+import twilio.twiml
+from twilio import *
+from twilio import twiml
+from twilio.util import RequestValidator
+from twilio.rest import TwilioRestClient
+from twilio import TwilioRestException
+from flask import Flask, request
+from flask import render_template
+from flask import g
+import time
+import sqlite3
+import json
+from multiprocessing import Process
+
+app = Flask(__name__)
+
+
+
+TWILIO_CALLER_ID = "+18312469407"
+TWILIO_ACCOUNT_SID = 'AC8fde0d1a6081058d9779798929b3623a'
+TWILIO_AUTH_TOKEN = '4de220ce7dfd39fe43ced795fbd15df4'
+url = "https://5839f837.ngrok.io/"
+
+
+def fizz_bizz(number):
+	lst = ["FizzBuzz" if i % 15 == 0 else "Fizz" if i % 3 == 0 else "Buzz" if i % 5 == 0 else str(i) for i in range(1,number+1)]
+	return " ".join(lst)
+
+def setup_database():
+    global connect
+    global cursor
+    DB = "database.db"
+    connect = sqlite3.connect(DB, check_same_thread=False)
+    cursor = connect.cursor()
+    table_exists = cursor.execute("SELECT name FROM sqlite_master WHERE name ='calls' and type='table'").fetchone()
+    
+    if not table_exists:
+        connect.execute('CREATE TABLE calls (id INTEGER PRIMARY KEY, datetime DATETIME, delay INT, phonenum INT, num INT)')
+    cursor.close()
+
+setup_database()
+
+@app.route('/', methods=['GET', 'POST']) #allow both GET and POST requests
+def main():
+    if request.method == 'POST':  #this block is only entered when the form is submitted
+        global phone
+        global delay
+        phone = request.form.get('phone')
+        delay = request.form['delay']
+        phone = phone.replace(" ","").replace("(","").replace(")","").replace("-","")
+        if not delay:
+            delay = 0
+        try:
+            assert(len(phone) == 10)
+            phone = int(phone)
+        except:
+            return '''<h1>Sorry {} is not a verified or valid phone number.</h1>
+                    <h2> Please input a valid number </h2>
+                  <a href={}>Return</a>'''.format(phone,url)  
+        try:
+            insert_into_sql(phone, delay)
+            cursor = connect.cursor()
+            id = cursor.execute("SELECT MAX(id) FROM calls")
+            id = id.fetchone()
+            id = id[0]
+            cursor.close()
+            p = Process(target=caller, args=(phone, delay, id))
+            p.start()
+            
+        except:
+            print('Error')
+        message = "Thank you for using PhoneBuzz! Your call to "+str(phone)+" has been initiated."
+        title = "Call Initiated"
+        return render_template('timer.html',call=title,delay=int(delay), message=message)   
+        
+    cursor = connect.cursor()
+    cursor.execute('SELECT * FROM calls')
+    return render_template('index.html', calls=cursor.fetchall())
+
+def insert_into_sql(phone, delay):
+    cursor = connect.cursor()
+    insert = "INSERT INTO calls (id, datetime, delay, phonenum) VALUES "
+    insert = insert + "(NULL, CURRENT_TIMESTAMP," + str(delay) + "," + str(phone) +" )"
+    cursor.execute(insert)
+    connect.commit()
+    cursor.close()
+
+def caller(phone, delay, id):
+    phone = str(phone)
+    if delay:
+       time.sleep(int(delay))
+    client = TwilioRestClient(TWILIO_ACCOUNT_SID,TWILIO_AUTH_TOKEN)
+    call = client.calls.create(to='+1'+phone,
+                               from_=TWILIO_CALLER_ID,
+                               url=url+"response?id="+str(id))
+    print(call)
+
+
+@app.route("/response", methods=['GET', 'POST'])
+def response():
+    resp = twilio.twiml.Response()
+
+    signature = request.headers.get('X-Twilio-Signature', '')
+    print(signature)
+    if not signature:
+        resp.say("This is not Twilio")
+        return str(resp)
+
+    id = request.args.get("id")
+    if id:
+        with resp.gather(finishOnKey="#", action="/handle-number?id="+str(id), method="POST") as g:
+            message = "Welcome to PhoneBuzz! Please enter any number, followed by the pound sign, to play!"
+            g.say(message, voice="woman")
+    else:
+        with resp.gather(finishOnKey="#", action="/handle-number", method="POST") as g:
+            message = "Welcome to PhoneBuzz! Please enter any number, followed by the pound sign, to play!"
+            g.say(message, voice="woman")
+
+    return str(resp)
+
+def replay_call(num, delay, phone):
+    client = TwilioRestClient(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
+    new_url = url + "replay-number?num=" + str(num)
+    print(phone)
+    time.sleep(delay)
+    call = client.calls.create(to='+1'+str(phone),
+                               from_=TWILIO_CALLER_ID,
+                               url=new_url)
+@app.route("/replay/<id>", methods=['GET', 'POST'])
+def replay(id):
+    cursor = connect.cursor()
+    phone = cursor.execute("SELECT phonenum FROM calls WHERE id=" + str(id))
+    phone = phone.fetchall()[0][0]
+    delay = cursor.execute("SELECT delay FROM calls WHERE id=" + str(id))
+    delay = delay.fetchall()[0][0]
+    num = cursor.execute("SELECT num FROM calls WHERE id=" + str(id))
+    num = num.fetchall()[0][0]
+    if not num:
+        cursor = connect.cursor()
+        cursor.execute("DELETE FROM calls WHERE num is NULL")
+        cursor.close()
+        return '''
+            <title>Recall Could Not Be Initiated</title>
+              <h1>There was no number stored for this replay. </h1>
+              <h2>This is probably because a user did not input a number or hung up. This field will now be deleted from the home page.</h2>
+              <a href=()>Return</a>'''.format(url)
+    cursor = connect.cursor()
+    insert = "INSERT INTO calls (id, datetime, delay, phonenum, num) VALUES "
+    insert = insert + "(NULL, CURRENT_TIMESTAMP," + str(delay) + "," + str(phone) + "," + str(num) + ")"
+    cursor.execute(insert)
+    connect.commit()
+    cursor.close()
+
+    p = Process(target=replay_call, args=(num, delay, phone))
+    p.start()
+    
+    message = "Thank you for using PhoneBuzz! Your recall to "+str(phone)+", with delay "+ str(delay) + " seconds has been initiated."
+    title = "Recall Initiated"
+    return render_template('timer.html', delay=int(delay),message=message,call=title)
+
+@app.route("/replay-number", methods=['GET', 'POST'])
+def replay_number():
+    number = request.args.get("num")
+    number = int(number)
+    
+    resp = twiml.Response()
+    message = "Welcome to PhoneBuzz Replay! The number is " + str(number) + "..."
+
+    message += fizz_bizz(number)
+    
+    message += "......Thank you for using PhoneBuzz!"   
+    
+    resp.say(message, voice="woman")
+    return str(resp)
+
+@app.route("/handle-number", methods=['GET', 'POST'])
+def handle_number():
+        
+    number_string = request.values.get('Digits', None)
+    number = int(number_string)
+    try:
+        ident = request.args.get('id')
+    except:
+        cursor = connect.cursor()
+        ident = cursor.execute("SELECT MAX(id) FROM calls")
+        ident = ident.fetchone()
+        ident = ident[0]
+        cursor.close()
+    cursor = connect.cursor()
+    stmt = "UPDATE calls SET num=" + str(number) + " WHERE id=" + str(ident)
+    print(stmt)
+    cursor.execute(stmt)
+    connect.commit()
+
+    resp = twilio.twiml.Response()
+    message = "..."
+    
+    lst = fizz_bizz(number)
+    
+    message += lst
+    message += "......Thank you for using PhoneBuzz!"
+    resp.say(message, voice="woman")
+    cursor = connect.cursor()
+    cursor.execute("DELETE FROM calls WHERE num is NULL")
+    cursor.close()
+    return str(resp)
+
+
+if __name__ == "__main__":
+	app.run(debug=True)
+
+
